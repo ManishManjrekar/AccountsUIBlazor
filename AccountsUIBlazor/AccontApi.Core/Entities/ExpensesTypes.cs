252be1356f1c9756d6ae62ca7ec1dd0ea24b60e4
Harshada <harshada.awari2@gmail.com>
2025-04-21 22:13:09 +0530

    change the some entity in table

--- AccountsUIBlazor/AccontApi.Core/Entities/ExpensesTypes.cs
+++ AccountsUIBlazor/AccontApi.Core/Entities/ExpensesTypes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,40 @@ namespace AccountApi.Core.Entities
        // public ExpensesTypes() { }
         public int ExpensesId { get; set; }
         public string ExpensesType { get; set; }
+
+        public int CustomerId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        //[Required]
+        public string MiddleName { get; set; }
+
+        //[Required]
+        public string NickName { get; set; }
+
+        //[Required]
+        public string LastName { get; set; }
+
+        //[Required]
+        // public string Email { get; set; }
+
+        [Required]
+        public string Mobile { get; set; }
+
+        public string ReferredBy { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        //public string Url { get; set; }
+
+        public bool IsActive { get; set; }
 
     }
 }
